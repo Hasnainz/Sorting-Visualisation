6454e9cd9a45cf28264fec942582edc668cb6306
unknown <zaidih@bedfordschool.org.uk>
2020-06-14 16:14:09 +0100

    working merge sort and fixed bar height issues

--- src/components/Algorithms/mergesort.js
+++ src/components/Algorithms/mergesort.js
@@ -1,6 +1,45 @@
 export default function getMergeSortAnimations(array){
-    let animations = [];
-    
+    const auxiliaryArray = array.slice();
+    const animations = []
+    if (array.length <= 1) return array;
+    mergeSort(array, auxiliaryArray, 0, array.length - 1, animations);
+    console.log(animations);
     return animations;
 }
-    
+function mergeSort(array, auxillaryArray, start, end, animations){
+    if(start === end) return;
+    const middle = Math.floor((start + end)/2);
+    mergeSort(auxillaryArray, array, start, middle, animations);
+    mergeSort(auxillaryArray, array, middle + 1, end, animations);
+    merge(array, auxillaryArray, start, end, middle, animations);
+}
+function merge(array, auxillaryArray, start, end, middle, animations){
+    let left = start;
+    let right = middle + 1;
+    let index = start;
+    console.log(array.length);
+    while(left <= middle && right <= end){
+        if(auxillaryArray[left] < auxillaryArray[right]){
+            animations.push([left, index, auxillaryArray[left]])
+            array[index] = auxillaryArray[left];
+            left++;
+        }else{
+            animations.push([right, index, auxillaryArray[right]])
+            array[index] = auxillaryArray[right];
+            right++;
+        }
+        index++;
+    }
+    while(left <= middle){
+        animations.push([left, index,auxillaryArray[left]])
+        array[index] = auxillaryArray[left];
+        left++;
+        index++;
+    }
+    while(right <= end){
+        animations.push([right, index, auxillaryArray[right]])
+        array[index] = auxillaryArray[right];
+        right++;
+        index++;
+    }
+}
--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -13,11 +13,12 @@ import getInsertionSortAnimations from './Algorithms/insertionsort';
 //#70b8c7 light blue
 //#9c9fb6 lilac
 //#383683 purple
-const primaryColour = "#6EA4BF"; //Air Superiority blue
+const primaryColour = "#70b8c7"; //Air Superiority blue
 const selectedColour = "#383683"; //Purple
 const successColour = "#97DB4F"; //Inchworm green
 const failedColour = "#D64933"; //Cinnabar
-let backgroundColour = primaryColour;
+const backgroundColour = "FFFFFF";
+let barColour = primaryColour;
 
 let size, pixelwidth, width, height, animationSpeed;
 
@@ -30,7 +31,7 @@ export default class Sort extends React.Component{
     this.onSizeSliderChange = this.onSizeSliderChange.bind(this);
     this.onSpeedSliderChange = this.onSpeedSliderChange.bind(this);
   }
-  
+  //On startup, this is what runs.
   componentDidMount(){
     window.addEventListener('resize', this.updateWindowDimensions);
     this.updateWindowDimensions();
@@ -39,16 +40,28 @@ export default class Sort extends React.Component{
     pixelwidth = Math.floor((width/(size*2)));
     this.resetArray();
   }
-
+  //On closing, this is what runs
   componentWillUnmount() {
     window.removeEventListener('resize', this.updateWindowDimensions);
   }
-  
+  //This is so that *hopefully* this will work on any sized screen as the compenents will be generated based on screen size
   updateWindowDimensions() {
     width =  (window.innerWidth*(0.9));
     height = (window.innerHeight*(0.9));
   }
-  async displayAnimations(animations){
+  async AnimationFinished(){
+    barColour = successColour;
+    this.forceUpdate();
+    await wait(1000);
+    barColour = primaryColour;
+    this.forceUpdate();
+
+  }
+  //Instead of returning a sorted array, these algorithms sort the array but return
+  //an array of the order in which the elements were sorted giving an animation array.
+  //This allows for features such as pausing and playing to be added with more ease.
+  async BubbleSort(){
+    let animations = getBubbleSortAnimations(this.state.array);
     const size = animations.length;
     let arrayBars = document.getElementsByClassName("array-bar");
     for(let i=0;i<size;i++){
@@ -56,9 +69,6 @@ export default class Sort extends React.Component{
 
       let barOneStyle = arrayBars[barOneIndex].style;
       let barTwoStyle = arrayBars[barTwoIndex].style;
-
-      console.log(arrayBars[barOneIndex].style.height);
-      console.log(arrayBars[barTwoIndex].style.height)
 
       barOneStyle.backgroundColor = selectedColour;
       barTwoStyle.backgroundColor = selectedColour;
@@ -81,50 +91,69 @@ export default class Sort extends React.Component{
       barOneStyle.backgroundColor = primaryColour;
       barTwoStyle.backgroundColor = primaryColour;
     }
-    backgroundColour = successColour;
-    this.forceUpdate();
-    await wait(1000);
-    backgroundColour = primaryColour;
-    this.forceUpdate();
+    this.AnimationFinished();
   }
 
-  BubbleSort(){
-    let animations = getBubbleSortAnimations(this.state.array);
-    this.displayAnimations(animations);
-  }
-
-  MergeSort(){
+  async MergeSort(){
     let animations = getMergeSortAnimations(this.state.array);
-    this.displayAnimations(animations);
+    let arrayBars = document.getElementsByClassName("array-bar");
+    const length = animations.length;
+    for(let i = 0; i < length; i++){
+      let [barOneIndex, barTwoIndex, barHeight] = animations[i];
+      if(barOneIndex === size) barOneIndex--;
+      if(barTwoIndex === size) barTwoIndex--;
+
+      let barOneStyle = arrayBars[barOneIndex].style;
+      let barTwoStyle = arrayBars[barTwoIndex].style;
+      barOneStyle.backgroundColor = selectedColour;
+      barTwoStyle.backgroundColor = selectedColour;
+      await wait(animationSpeed);
+      arrayBars[barTwoIndex].style.height = `${barHeight}px`;
+      await wait(animationSpeed);
+      barOneStyle.backgroundColor = primaryColour;
+      barTwoStyle.backgroundColor = primaryColour;
+      
+    }
+    this.AnimationFinished();
   }
 
-  HeapSort(){
-    let animations = getHeapSortAnimations(this.state.array);
-    this.displayAnimations(animations);
+  async HeapSort(){
   }
 
-  QuickSort(){
-    let animations = getQuickSortAnimations(this.state.array);
-    this.displayAnimations(animations);
+  async QuickSort(){
   }
-  InsertionSort(){
-    let animations = getInsertionSortAnimations(this.state.array);
-    this.displayAnimations(animations);
+  async InsertionSort(){
   }
   
   resetArray(){
-    const array = [];
-    for (let i = 0; i < size; i++){
+    let array = [];
+    for (let i = 0; i < size-1; i++){
       array.push(getRandomNumber(10, height));
     }
     this.setState({ array });
   }
+  
+  shuffle(array) {
+    //Fisher-Yates Shuffle - most time efficient shuffle algorithm which is important 
+    //for when you are generating this many arrays.
+    let counter = array.length;
+    while (counter > 0) {
+        let index = Math.floor(Math.random() * counter);
+        counter--;
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+    return array;
+}
 
   onSizeSliderChange(event){
     size = event.target.value;
     pixelwidth = Math.floor((width/(size*2)));
+    if(pixelwidth < 1) pixelwidth = 1;
     this.resetArray()
   }
+  
   onSpeedSliderChange(event){
     animationSpeed = 1000 - event.target.value;
   }
@@ -135,11 +164,18 @@ export default class Sort extends React.Component{
     return (
       <div className="total-container">
         <div className="array-container">
+          <div className="not-array-bar"
+              style={{
+              backgroundColor: backgroundColour,
+              height: `${height}px`,
+              width: `${20}px`,
+              }}
+          ></div>
           {array.map((value, index) => (
             <div className="array-bar"
               key={index}
               style={{
-                backgroundColor: backgroundColour,
+                backgroundColor: barColour,
                 height: `${value}px`,
                 width: `${pixelwidth}px`,
               }}></div>
@@ -160,7 +196,7 @@ export default class Sort extends React.Component{
             class="size-slider"
             type="range" 
             min="4"
-            max="300"
+            max="275"
             value={this.state.value}
             onChange={this.onSizeSliderChange}>
             </input>
