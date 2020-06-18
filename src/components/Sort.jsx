a68741cc9648d0a8a28b71e7da990cf2048de086
unknown <zaidih@bedfordschool.org.uk>
2020-06-18 12:11:07 +0100

    insertion sort and added stopping feature

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -11,6 +11,7 @@ const selectedColour = "#383683"; //Purple
 const successColour = "#97DB4F"; //Inchworm green
 const failedColour = "#D64933"; //Cinnabar
 const backgroundColour = "FFFFFF";
+let isRunning = false;
 let barColour = primaryColour;
 let size, pixelwidth, width, height, animationSpeed;
 
@@ -25,7 +26,7 @@ export default class Sort extends React.Component{
   }
   //On startup, this is what runs.
   componentDidMount(){
-    window.addEventListener('resize', this.updateWindowDimensions);
+    window.addEventListener('resize', this.updateWindowDimensions());
     this.updateWindowDimensions();
     size = 100;
     animationSpeed = 100;
@@ -34,12 +35,28 @@ export default class Sort extends React.Component{
   }
   //On closing, this is what runs
   componentWillUnmount() {
-    window.removeEventListener('resize', this.updateWindowDimensions);
+    window.removeEventListener('resize', this.updateWindowDimensions());
   }
   //This is so that *hopefully* this will work on any sized screen as the compenents will be generated based on screen size
   updateWindowDimensions() {
     width =  (window.innerWidth*(0.9));
     height = (window.innerHeight*(0.9));
+  }
+  StartSorting(){
+    isRunning = true;
+    let buttons = document.getElementsByClassName("disabledbutton");
+    for(let button of buttons){
+      button.disabled = true;
+    }
+  }
+  StopRunning(){
+    isRunning = false;
+    const arr = this.state.array;
+    this.setState({ arr });
+    let buttons = document.getElementsByClassName("disabledbutton");
+    for(let button of buttons){
+      button.disabled = false;
+    }
   }
   async AnimationFinished(){
     barColour = successColour;
@@ -56,32 +73,38 @@ export default class Sort extends React.Component{
     let animations = getBubbleSortAnimations(this.state.array.slice());
     const size = animations.length;
     let arrayBars = document.getElementsByClassName("array-bar");
+    this.StartSorting();
     for(let i=0;i<size;i++){
-      let [barOneIndex, barTwoIndex] = animations[i]
+      if(isRunning){
+        let [barOneIndex, barTwoIndex] = animations[i]
 
-      let barOneStyle = arrayBars[barOneIndex].style;
-      let barTwoStyle = arrayBars[barTwoIndex].style;
-
-      barOneStyle.backgroundColor = selectedColour;
-      barTwoStyle.backgroundColor = selectedColour;
-      await wait(animationSpeed);
-
-      if(parseInt(arrayBars[barOneIndex].style.height) > parseInt(arrayBars[barTwoIndex].style.height)){
-
-        let temp = arrayBars[barTwoIndex].style.height;
-        arrayBars[barTwoIndex].style.height = arrayBars[barOneIndex].style.height;
-        arrayBars[barOneIndex].style.height = temp;
-
-        barOneStyle.backgroundColor = successColour;
-        barTwoStyle.backgroundColor = successColour;
+        let barOneStyle = arrayBars[barOneIndex].style;
+        let barTwoStyle = arrayBars[barTwoIndex].style;
+  
+        barOneStyle.backgroundColor = selectedColour;
+        barTwoStyle.backgroundColor = selectedColour;
         await wait(animationSpeed);
-      }else{
-        barOneStyle.backgroundColor = failedColour;
-        barTwoStyle.backgroundColor = failedColour;
-        await wait(animationSpeed);
+  
+        if(parseInt(arrayBars[barOneIndex].style.height) > parseInt(arrayBars[barTwoIndex].style.height)){
+  
+          let temp = arrayBars[barTwoIndex].style.height;
+          arrayBars[barTwoIndex].style.height = arrayBars[barOneIndex].style.height;
+          arrayBars[barOneIndex].style.height = temp;
+  
+          barOneStyle.backgroundColor = successColour;
+          barTwoStyle.backgroundColor = successColour;
+          await wait(animationSpeed);
+        }else{
+          barOneStyle.backgroundColor = failedColour;
+          barTwoStyle.backgroundColor = failedColour;
+          await wait(animationSpeed);
+        }
+        barOneStyle.backgroundColor = primaryColour;
+        barTwoStyle.backgroundColor = primaryColour;
       }
-      barOneStyle.backgroundColor = primaryColour;
-      barTwoStyle.backgroundColor = primaryColour;
+      else{
+        return;
+      }
     }
     this.AnimationFinished();
   }
@@ -90,21 +113,26 @@ export default class Sort extends React.Component{
     let animations = getMergeSortAnimations(this.state.array.slice());
     let arrayBars = document.getElementsByClassName("array-bar");
     const length = animations.length;
+    this.StartSorting();
     for(let i = 0; i < length; i++){
-      let [barOneIndex, barTwoIndex, barHeight] = animations[i];
-      if(barOneIndex === size) barOneIndex--;
-      if(barTwoIndex === size) barTwoIndex--;
-
-      let barOneStyle = arrayBars[barOneIndex].style;
-      let barTwoStyle = arrayBars[barTwoIndex].style;
-      barOneStyle.backgroundColor = selectedColour;
-      barTwoStyle.backgroundColor = selectedColour;
-      await wait(animationSpeed);
-      arrayBars[barTwoIndex].style.height = `${barHeight}px`;
-      await wait(animationSpeed);
-      barOneStyle.backgroundColor = primaryColour;
-      barTwoStyle.backgroundColor = primaryColour;
-      
+      if(isRunning){
+        let [barOneIndex, barTwoIndex, barHeight] = animations[i];
+        if(barOneIndex === size) barOneIndex--;
+        if(barTwoIndex === size) barTwoIndex--;
+  
+        let barOneStyle = arrayBars[barOneIndex].style;
+        let barTwoStyle = arrayBars[barTwoIndex].style;
+        barOneStyle.backgroundColor = selectedColour;
+        barTwoStyle.backgroundColor = selectedColour;
+        await wait(animationSpeed);
+        arrayBars[barTwoIndex].style.height = `${barHeight}px`;
+        await wait(animationSpeed);
+        barOneStyle.backgroundColor = primaryColour;
+        barTwoStyle.backgroundColor = primaryColour;
+      }
+      else{
+        return;
+      }
     }
     await this.AnimationFinished();
   }
@@ -113,30 +141,13 @@ export default class Sort extends React.Component{
     let animations = getHeapSortAnimations(this.state.array.slice());
     const length = animations.length;
     let arrayBars = document.getElementsByClassName("array-bar");
+    this.StartSorting();
     for(let i = 0; i < length; i++){
-      let [barOneIndex, barTwoIndex, barOneHeight, barTwoHeight] = animations[i];   
-      arrayBars[barOneIndex].style.backgroundColor = selectedColour;
-      arrayBars[barTwoIndex].style.backgroundColor = selectedColour;
-      await wait(animationSpeed);
-      arrayBars[barOneIndex].style.height = `${barTwoHeight}px`;
-      arrayBars[barTwoIndex].style.height = `${barOneHeight}px`;
-      arrayBars[barOneIndex].style.backgroundColor = successColour;
-      arrayBars[barTwoIndex].style.backgroundColor = successColour;
-      await wait(animationSpeed);
-      arrayBars[barOneIndex].style.backgroundColor = primaryColour;
-      arrayBars[barTwoIndex].style.backgroundColor = primaryColour;
-    }
-    await this.AnimationFinished();
-  }
-
-  async QuickSort(){
-    let animations = getQuickSortAnimations(this.state.array.slice());
-    const length = animations.length;
-    let arrayBars = document.getElementsByClassName("array-bar");
-    console.log(animations);
-    for(let i = 0; i < length; i++){
-      if(animations[i].length === 4){
+      if(isRunning){
         let [barOneIndex, barTwoIndex, barOneHeight, barTwoHeight] = animations[i];   
+        arrayBars[barOneIndex].style.backgroundColor = selectedColour;
+        arrayBars[barTwoIndex].style.backgroundColor = selectedColour;
+        await wait(animationSpeed);
         arrayBars[barOneIndex].style.height = `${barTwoHeight}px`;
         arrayBars[barTwoIndex].style.height = `${barOneHeight}px`;
         arrayBars[barOneIndex].style.backgroundColor = successColour;
@@ -145,26 +156,82 @@ export default class Sort extends React.Component{
         arrayBars[barOneIndex].style.backgroundColor = primaryColour;
         arrayBars[barTwoIndex].style.backgroundColor = primaryColour;
       }
-      else if(animations[i].length === 2){
-        if(i !== 0){
-          let [previousPivot, notpivot] = animations[i-1];
-          arrayBars[previousPivot].style.backgroundColor = primaryColour;
+      else{
+        return;
+      }
+    }
+    await this.AnimationFinished();
+  }
+
+  async QuickSort(){
+    let animations = getQuickSortAnimations(this.state.array.slice());
+    const length = animations.length;
+    let arrayBars = document.getElementsByClassName("array-bar");
+    let tempPivotIndex = 0;
+    this.StartSorting();
+    for(let i = 0; i < length; i++){
+      if(isRunning){
+        if(animations[i].length === 4){
+          let [barOneIndex, barTwoIndex, barOneHeight, barTwoHeight] = animations[i];   
+          arrayBars[barOneIndex].style.height = `${barTwoHeight}px`;
+          arrayBars[barTwoIndex].style.height = `${barOneHeight}px`;
+          arrayBars[barOneIndex].style.backgroundColor = successColour;
+          arrayBars[barTwoIndex].style.backgroundColor = successColour;
+          await wait(animationSpeed);
+          arrayBars[barOneIndex].style.backgroundColor = primaryColour;
+          arrayBars[barTwoIndex].style.backgroundColor = primaryColour;
         }
-        let [pivotIndex, pivot] = animations[i];
-        arrayBars[pivotIndex].style.backgroundColor = selectedColour;
-        await wait(animationSpeed);
+        else if(animations[i].length === 2){
+          if(i !== 0){
+            let [previousPivot, notpivot] = animations[i-1];
+            arrayBars[previousPivot].style.backgroundColor = primaryColour;
+          }
+          let [pivotIndex, pivot] = animations[i];
+          arrayBars[pivotIndex].style.backgroundColor = selectedColour;
+          await wait(animationSpeed);
+        }
+        else{
+          let [barOneIndex] = animations[i];
+          tempPivotIndex = barOneIndex;
+          arrayBars[barOneIndex].style.backgroundColor = selectedColour;
+          await wait(animationSpeed);
+          arrayBars[barOneIndex].style.backgroundColor = primaryColour;
+        }  
       }
       else{
-        let [barOneIndex] = animations[i];
-        arrayBars[barOneIndex].style.backgroundColor = selectedColour;
-        await wait(animationSpeed);
-        arrayBars[barOneIndex].style.backgroundColor = primaryColour;
+        arrayBars[tempPivotIndex].style.backgroundColor = primaryColour;
+        return;
       }
-  
     }
     await this.AnimationFinished();
   }
   async InsertionSort(){
+    let animations = getInsertionSortAnimations(this.state.array.slice());
+    const length = animations.length;
+    let arrayBars = document.getElementsByClassName("array-bar");
+    this.StartSorting();
+    for(let i = 0; i < length; i++){
+      if(isRunning){
+        if(animations[i].length === 3){
+          let [barOneIndex, barTwoIndex, barOneHeight] = animations[i];
+          arrayBars[barOneIndex].style.backgroundColor = selectedColour;
+          arrayBars[barTwoIndex].style.backgroundColor = selectedColour;
+          arrayBars[barOneIndex+1].style.height = `${barOneHeight}px`;
+          await wait(animationSpeed);
+          arrayBars[barOneIndex].style.backgroundColor = primaryColour;
+          arrayBars[barTwoIndex].style.backgroundColor = primaryColour;
+        }
+        else{
+          let [barOneIndex, keyHeight, turn] = animations[i];
+          arrayBars[barOneIndex].style.height = `${keyHeight}px`;
+          await wait(animationSpeed);
+        }
+      }
+      else{
+        return;
+      }
+    }
+    await this.AnimationFinished();
   }
   
   resetArray(){
@@ -225,12 +292,13 @@ export default class Sort extends React.Component{
         </div>
         <div className="footer-container">
           <div>
-              <button onClick={() => this.resetArray()}>Generate New Array</button>
-              <button onClick={() => this.MergeSort()}>Merge Sort</button>
-              <button onClick={() => this.BubbleSort()}>Bubble Sort</button>
-              <button onClick={() => this.HeapSort()}>Heap Sort</button>
-              <button onClick={() => this.QuickSort()}>Quick Sort</button>
-              <button onClick={() => this.InsertionSort()}>Insertion Sort</button>
+              <button className="disabledbutton" onClick={() => this.resetArray()}>Generate New Array</button>
+              <button onClick={() => this.StopRunning()}>Stop</button>
+              <button className="disabledbutton" onClick={() => this.MergeSort()}>Merge Sort</button>
+              <button className="disabledbutton" onClick={() => this.BubbleSort()}>Bubble Sort</button>
+              <button className="disabledbutton" onClick={() => this.HeapSort()}>Heap Sort</button>
+              <button className="disabledbutton" onClick={() => this.QuickSort()}>Quick Sort</button>
+              <button className="disabledbutton" onClick={() => this.InsertionSort()}>Insertion Sort</button>
           </div>
           <div>
             <label>Size &nbsp;</label>
