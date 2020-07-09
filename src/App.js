083e11fd54bd3a298d083541194b8d74aebb0363
unknown <zaidih@bedfordschool.org.uk>
2020-07-09 18:39:47 +0100

    hash

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Sort from "./components/Sort";
 import Runtimes from "./components/Runtimes";   
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App()
 {
