ee8df0c194676e57f28163976aac0292228c533e
unknown <zaidih@bedfordschool.org.uk>
2020-10-24 23:15:50 +0100

    Updates

---
+++ precache-manifest.4535cc717b30453742fc934707fccdd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec022c4db991c619d54dc2cc52479be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36119e2b7b2f4a0d1133",
+    "url": "/static/css/main.8203e6c4.chunk.css"
+  },
+  {
+    "revision": "d86b19acf6cb6e35b04f",
+    "url": "/static/js/2.f2a57506.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f2a57506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36119e2b7b2f4a0d1133",
+    "url": "/static/js/main.4956cd92.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sorting-Visualisation/precache-manifest.f4a66d1f0ad7c3fc573acc96e55c3422.js"
+  "/precache-manifest.4535cc717b30453742fc934707fccdd5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sorting-Visualisation/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
